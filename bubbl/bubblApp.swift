881432767eba2def7a2459d16dbc24a77c121b30
Edgar Dollentas <edgardollentas04@gmail.com>
2026-05-26 03:26:48 +0800

    first commit

---
+++ bubbl/bubblApp.swift
@@ -0,0 +1,17 @@
+//
+//  bubblApp.swift
+//  bubbl
+//
+//  Created by Eli on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct bubblApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
